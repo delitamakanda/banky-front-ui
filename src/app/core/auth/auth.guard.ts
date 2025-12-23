6f8d3da6be4f80863975183a8adf6dd36508df28
Délita Makanda <delita.makanda@gmail.com>
2025-12-23 22:10:06 +0100

    refactor auth guard to simplify logic and improve readability

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -2,16 +2,13 @@ import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import {AuthStore} from './auth.store';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (route, state) => {
   const auth = inject(AuthStore)
   const router = inject(Router);
 
-  if (auth.isAuthenticated()) {
-    return true;
+  if (!auth.isAuthenticated()) {
+    router.navigate(['/login']);
+    return false;
   }
-
-  router.navigate(['login']);
-
-  return false;
-
+  return true;
 }
